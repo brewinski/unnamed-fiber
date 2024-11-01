b2f4139896c1eab78ff3daed5db62b56f2e90703
Chris Brewin Local <chris@LP4570.local>
2024-11-01 16:58:56 +1000

    update this thing

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
@@ -8,6 +10,7 @@ import (
 
 // SetupRoutes func to setup routes
 func SetupRoutes(app *fiber.App) {
+	fmt.Println("testing")
 
 	api := app.Group("api", logger.New(), recover.New(recover.Config{
 		EnableStackTrace: true,
